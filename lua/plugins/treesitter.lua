857a0fdddb6f2f87eb5cdf74591883f898a767b9
Pouria Memarvar <pouria.memarvar@designa.com>
2024-07-12 16:32:28 +0200

    add svelte to treesitter highlighting

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,11 +1,11 @@
 return {
-  "nvim-treesitter/nvim-treesitter", 
+  "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   config = function()
     local config = require("nvim-treesitter.configs")
 
     config.setup({
-      ensure_installed = {"lua", "javascript", "dockerfile", "angular", "html", "css", "typescript"},
+      ensure_installed = { "svelte", "lua", "javascript", "dockerfile", "angular", "html", "css", "typescript" },
       highlight = { enable = true },
       indent = { enable = true },
     })
