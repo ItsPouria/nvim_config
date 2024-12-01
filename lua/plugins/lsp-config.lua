81d478d85fc4fc5748f9718ce43f222b33ef48bb
ItsPouria <pouriamemarvar@gmail.com>
2024-12-01 23:40:19 +0100

    add rust_analyzer to lsp config

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -15,6 +15,32 @@ return {
   },
   {
     "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        rust_analyzer = {
+          mason = false,
+          cmd = { vim.fn.expand("~/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/bin/rust-analyzer") },
+          settings = {
+            ["rust-analyzer"] = {
+              imports = {
+                granularity = {
+                  group = "module",
+                },
+                prefix = "self",
+              },
+              cargo = {
+                buildScripts = {
+                  enable = true,
+                },
+              },
+              procMacro = {
+                enable = true,
+              },
+            },
+          },
+        },
+      },
+    },
     lazy = false,
     config = function()
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
