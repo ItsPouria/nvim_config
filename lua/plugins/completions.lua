405b84d1d11ee4a6974e33258fe00ad2f9ac223d
Pouria Memarvar <pouria.memarvar@designa.com>
2025-02-19 20:54:10 +0100

    fix autocomplete snippets

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,55 +1,44 @@
 return {
   {
-    'hrsh7th/cmp-nvim-lsp',
-    event = 'InsertEnter',
+    "hrsh7th/cmp-nvim-lsp"
+  },
+  {
+    "L3MON4D3/LuaSnip",
     dependencies = {
-      'hrsh7th/cmp-buffer',
-      'hrsh7th/cmp-path',
-      'L3MON4D3/LuaSnip',
-      'saadparwaiz1/cmp_luasnip',
-      'rafamadriz/friendly-snippets',
-      'onsails/lspkind.nvim',
-      'hrsh7th/nvim-cmp',
-
+      "saadparwaiz1/cmp_luasnip",
+      "rafamadriz/friendly-snippets",
     },
-
+  },
+  {
+    "hrsh7th/nvim-cmp",
     config = function()
-      local cmp = require('cmp')
-      local luasnip = require('luasnip')
-      local lspkind = require('lspkind')
-
-      require('luasnip.loaders.from_vscode').lazy_load()
+      local cmp = require("cmp")
+      require("luasnip.loaders.from_vscode").lazy_load()
 
       cmp.setup({
-        completion = {
-          completeop = 'menu,menuone,preview,noselect',
-        },
         snippet = {
           expand = function(args)
-            luasnip.lsp_examp(args.body)
+            require("luasnip").lsp_expand(args.body)
           end,
         },
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
         mapping = cmp.mapping.preset.insert({
-          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<C-Space>'] = cmp.mapping.complete(),
-          ['<C-e>'] = cmp.mapping.abort(),
-          ['<CR>'] = cmp.mapping.confirm({ select = true }),
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
         }),
         sources = cmp.config.sources({
-          { name = 'nvim_lsp' },
-          { name = 'luasnip' },
-          { name = 'buffer' },
-          { name = 'path' },
+          { name = "nvim_lsp" },
+          { name = "luasnip" }, -- For luasnip users.
+        }, {
+          { name = "buffer" },
         }),
-
-        formatting = {
-          format = lspkind.cmp_format({
-            maxwidth = 50,
-            ellipsis_char = '...',
-          }),
-        },
       })
-    end
-  }
+    end,
+  },
 }
